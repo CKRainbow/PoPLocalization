d116e2e41010e94f9e4a88afdd042fc62abfdf0a
CK Rainbow <nanashichi@proton.me>
2025-10-09 08:01:20 +0800

    fix: try to fix the bug from original game

--- decompile_fix.py
+++ decompile_fix.py
@@ -1,16 +1,45 @@
 from pathlib import Path
+from dataclasses import dataclass
+
+@dataclass
+class FixRule:
+    path: str
+    fix_rules: list[tuple[str, str]]
+
+FIXRULE = []
+
+FIXRULE.append(
+    FixRule(
+        path="SaveLoadManager.cs",
+        fix_rules=[
+            ("ref string reference;", "ref string reference = ref array[0];"),
+        ],
+    )
+)
+
+FIXRULE.append(
+    FixRule(
+        path="OverWorldController.cs",
+        fix_rules=[
+            ("if (randomNonHazardousValidTile.worldObject == OWTile.WorldObject.None)", 
+             "if (randomNonHazardousValidTile is not null && randomNonHazardousValidTile.worldObject == OWTile.WorldObject.None)")
+        ],
+    )
+)
 
 def decompile_fix(directory: Path):
+    
+    for fix_rule in FIXRULE:
+        target_file = directory / fix_rule.path
 
-    target_file = directory / "SaveLoadManager.cs"
+        with open(target_file, "r", encoding="utf-8") as f:
+            text = f.read()
 
-    with open(target_file, "r", encoding="utf-8") as f:
-        text = f.read()
+        for fix_rule in fix_rule.fix_rules:
+            text = text.replace(*fix_rule)
 
-    text = text.replace("ref string reference;", "ref string reference = ref array[0];")
-
-    with open(target_file, "w", encoding="utf-8") as f:
-        f.write(text)
+        with open(target_file, "w", encoding="utf-8") as f:
+            f.write(text)
 
 if __name__ == "__main__":
     decompile_path = Path("./decompiled")
